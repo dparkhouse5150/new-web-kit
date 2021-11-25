e720bf9158eee80c0c9874815975dde7b2571d90
darrellparkhouse5150 <86165927+darrellparkhouse5150@users.noreply.github.com>
2021-11-24 18:07:18 -0600

    added the UserComponent and ProfileComponent  routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ProfileComponent} from "./profile/profile.component";
+import {UserComponent} from "./user/user.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'user/profile/:id',
+        component: ProfileComponent
+    },
+    {
+        path: 'user',
+        component: UserComponent
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
